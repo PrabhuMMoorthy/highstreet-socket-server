3de528aff1a7816923d192f610dbc883c92308df
PrabhuMMoorthy <mprabhumoorthy@gmail.com>
2017-08-28 14:09:33 +0000

    Update socket-server.py

--- socket-server.py
+++ socket-server.py
@@ -7,6 +7,31 @@ class HelloHandler(tornado.web.RequestHandler):
   def get(self):
     self.write('Hello from tornado')
 
+class MyWebSocket(tornado.websocket.WebSocketHandler):
+  clients = []
+
+  def check_origin(self, origin):
+    return True
+
+  def open(self):
+    # clients must be accessed through class object!!!
+    MyWebSocket.clients.append(self)
+    print("WebSocket opened")
+
+  def on_message(self, message):
+    print("msg recevied", message)
+    msg = json.loads(message) # todo: safety?
+
+    # send other clients this message
+    for c in MyWebSocket.clients:
+      if c != self:
+        c.write_message(msg)
+
+  def on_close(self):
+    print("WebSocket closed")
+    # clients must be accessed through class object!!!
+    MyWebSocket.clients.remove(self)
+
 def main():
   tornado_app = tornado.web.Application([
       (r"/", HelloHandler),
